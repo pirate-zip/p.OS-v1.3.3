34550ccb5652a22e1aeb42be85616c7af6a51ad5
pirate-zip <121468706+pirate-zip@users.noreply.github.com>
2023-08-20 00:27:27 +0000

    fixed credits lol

--- src/commands/tools/credits.js
+++ src/commands/tools/credits.js
@@ -14,7 +14,7 @@ module.exports = {
       const {channel, options} = interaction;
 
     const creditEmbed = new EmbedBuilder()
-      .setDescription(`**Bot Credits**\n\nCreator: pirate_zip\nOther contributors: ShinyDuck21, HappyHat_47\nLanguage: Javascript\n24/7 server: UptimeRobot (https://www.uptimerobot.com)\n\np.OS is still in development and may contain bugs, especially in the prerelease versions. Please alert pirate_zip#2464 with any bug finds\n**p.OS current version: 1.3.3**`)
+      .setDescription(`**Bot Credits**\n\nCreator: pirate_zip\nOther contributors: ShinyDuck21, HappyHat_47, AGNAB\nLanguage: Javascript\n24/7 server: UptimeRobot (https://www.uptimerobot.com)\n\np.OS is still in development and may contain bugs, especially in the prerelease versions. Please alert pirate_zip with any bug finds\n**p.OS current version: 1.3.3**`)
     await interaction.reply({
       embeds: [creditEmbed]
     });
